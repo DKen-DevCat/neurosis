7e4c7c9002e1fd47247dc8a2186ae273c5fb16df
DKen-DevCat <dken.devdev@gmail.com>
2025-07-24 19:04:09 +0900

    fix(frontend): resolve quotation errors in jest config and test file

--- front-end/mobile-app/jest.config.js
+++ front-end/mobile-app/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  preset: 'jest-expo',
+  preset: "jest-expo",
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|nativewind)'
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|nativewind)"
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/__mocks__/styleMock.js',
+    "\\.css$": "<rootDir>/__mocks__/styleMock.js",
   },
 };
